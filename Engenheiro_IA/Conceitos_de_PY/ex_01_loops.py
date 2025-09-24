a106a3311289a56ebb11d51ad5a2db9f7c8b91dc
Pedro_Macbook <pedrocarregado15@gmail.com>
2025-09-24 20:34:40 +0100

    fim do dia 24/set renomear arquivos e exercicios de loops

---
+++ Engenheiro_IA/Conceitos_de_PY/ex_01_loops.py
@@ -0,0 +1,40 @@
+#Repetir ate ser verdadeiro WHILE
+n=1
+
+while n<=10:
+    print("*")
+    n = n+1
+
+
+print("\n##############################\n")
+
+
+lista_nomes=["Lucas", "Rafael", "Mariana", "Beatriz", "João"]
+lista_medias=[8.9, 7.5,4.2,1.4,9.5]
+
+#Somar 1 valor ás notas 
+#Se passar dos 10 não somo nenhum valor 
+n=0
+while n < 5:
+    if lista_medias[n] <=9:
+        lista_medias[n] = lista_medias[n]+1
+    
+    n=n+1
+
+print(lista_medias)
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+    
